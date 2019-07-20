a3296226744948ba192835af44d51dccf5676821
Nilank <sharmanilank.com>
2019-07-20 18:49:37 -0400

    Leet Code Valid Parentheses

---
+++ CrackingTheCodingInterview/src/com/nilank/crackingthecodinginterview/ValidParentheses/ValidParentheses.java
@@ -0,0 +1,17 @@
+
+package com.nilank.crackingthecodinginterview.ValidParentheses;
+
+/**
+ *
+ * @author nilan
+ */
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        String pattern = "(]";
+        Solution sol = new Solution();
+        System.out.println(sol.isValid(pattern));
+        
+    }
+    
+}
