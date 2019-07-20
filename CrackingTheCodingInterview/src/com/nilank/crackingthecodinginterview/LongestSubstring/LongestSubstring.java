bd077387bd7e712297a4b5353e159db013648bd0
Nilank <sharmanilank.com>
2019-07-20 19:40:14 -0400

    Leet Code Length Of Longest Substring

---
+++ CrackingTheCodingInterview/src/com/nilank/crackingthecodinginterview/LongestSubstring/LongestSubstring.java
@@ -0,0 +1,17 @@
+
+package com.nilank.crackingthecodinginterview.LongestSubstring;
+
+/**
+ *
+ * @author nilan
+ */
+public class LongestSubstring {
+
+    public static void main(String[] args) {
+        String input = "pwwkew";
+        Solution sol = new Solution();
+        System.out.println(sol.lengthOfLongestSubstring(input));
+        
+    }
+    
+}
