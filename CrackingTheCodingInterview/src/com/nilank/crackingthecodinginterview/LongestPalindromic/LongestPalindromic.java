6031cbf248852df5effa404eac047b0a8c02b1a7
Nilank <sharmanilank.com>
2019-07-21 12:41:44 -0400

    Leet Code Palindrome Longest String

---
+++ CrackingTheCodingInterview/src/com/nilank/crackingthecodinginterview/LongestPalindromic/LongestPalindromic.java
@@ -0,0 +1,15 @@
+
+package com.nilank.crackingthecodinginterview.LongestPalindromic;
+
+/**
+ *
+ * @author nilan
+ */
+public class LongestPalindromic {
+
+    public static void main(String[] args) {
+        String input = "babad";
+        Solution sol = new Solution();
+        System.out.println(sol.longestPalindrome(input));           
+    }
+}
